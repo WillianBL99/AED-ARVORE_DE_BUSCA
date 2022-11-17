ec9ce45ce8e310b68fd347b41a648a51b9b46c68
paulo uilian <uilianlbarros@gmail.com>
2022-11-17 15:37:50 -0300

    :sparkles:feat: implementa inserir valor na arvore

--- main.cpp
+++ main.cpp
@@ -9,10 +9,22 @@ typedef struct No {
   struct No *esq;
 } No;
 
-typedef struct No *Arvore;
+typedef struct Arvore {
+  No *raiz;
+} Arvore;
 
-Arvore *inicializarArvore(Arvore *arvore) {
-    return NULL;
+Arvore *inicializaArvore(Arvore *arvore) {
+  arvore = (Arvore *) malloc(sizeof(Arvore));
+  arvore->raiz = NULL;
+  return arvore;
+}
+
+No *criaNovoNo(int info) {
+    No *novoNo = (No *) malloc(sizeof(No));
+    novoNo->info = info;
+    novoNo->dir = NULL;
+    novoNo->esq = NULL;
+    return novoNo;
 }
 
 bool ehNull(No *no) {
@@ -34,8 +46,7 @@ No* buscar(No *raiz, int valor) {
       return NULL;
 
     } else {
-      noAtual = noAtual->esq;
-      buscar(noAtual, valor);
+      noAtual = buscar(noAtual->esq, valor);
     }
 
   } else {
@@ -43,25 +54,58 @@ No* buscar(No *raiz, int valor) {
       return NULL;
       
     } else {
-      noAtual = noAtual->dir;
-      buscar(noAtual, valor);
+      noAtual = buscar(noAtual->dir, valor);
     }
   }
 
-  return NULL;
+  return noAtual;
+}
+
+void inserir(Arvore* arvore, int valor) {
+  No *novoNo = criaNovoNo(valor);
+  
+  No *noAtual = arvore->raiz;
+
+  if(ehNull(noAtual)) {
+    arvore->raiz = novoNo;
+    return;
+  }
+
+  if(!ehNull(buscar(noAtual, valor))) {
+    cout << "Valor já existe na árvore" << endl;
+    return;
+  }
+
+  while(true) {
+    if(valor < noAtual->info) {
+      if(ehNull(noAtual->esq)) {
+        noAtual->esq = novoNo;
+        return;
+      } else {
+        noAtual = noAtual->esq;
+      }
+
+    } else {
+      if(ehNull(noAtual->dir)) {
+        noAtual->dir = novoNo;
+        return;
+      } else {
+        noAtual = noAtual->dir;
+      }
+    }
+  }
 }
 
 
 int main() {
   Arvore *arvore;
-  arvore = inicializarArvore(arvore);
+  arvore = inicializaArvore(arvore);
 
-  if(buscar((No*)arvore, 10) == NULL) {
+  if(buscar(arvore->raiz, 10) == NULL) {
     cout << "Valor não encontrado" << endl;
   } else {
     cout << "Valor encontrado" << endl;
   }
-
 
   return 0;
 }
